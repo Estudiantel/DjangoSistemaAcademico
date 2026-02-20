21cdd30820a15cb15a87979c05a948010842ad10
Fabio <gomezsecundariatecnica@gmail.com>
2026-02-19 23:40:55 -0300

    Implementa CRUDs CBV y frontend Bootstrap para gestión académica

--- sistema_academico/urls.py
+++ sistema_academico/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(pattern_name='usuarios:carrera-list', permanent=False)),
+    path('', include('usuarios.urls')),
 ]
---
+++ usuarios/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'usuarios'
+
+urlpatterns = [
+    path('carreras/', views.CarreraListView.as_view(), name='carrera-list'),
+    path('carreras/nueva/', views.CarreraCreateView.as_view(), name='carrera-create'),
+    path('carreras/<int:pk>/editar/', views.CarreraUpdateView.as_view(), name='carrera-update'),
+    path('carreras/<int:pk>/eliminar/', views.CarreraDeleteView.as_view(), name='carrera-delete'),
+    path('materias/', views.MateriaListView.as_view(), name='materia-list'),
+    path('materias/nueva/', views.MateriaCreateView.as_view(), name='materia-create'),
+    path('materias/<int:pk>/editar/', views.MateriaUpdateView.as_view(), name='materia-update'),
+    path('materias/<int:pk>/eliminar/', views.MateriaDeleteView.as_view(), name='materia-delete'),
+    path('alumnos/', views.AlumnoListView.as_view(), name='alumno-list'),
+    path('alumnos/nuevo/', views.AlumnoCreateView.as_view(), name='alumno-create'),
+    path('alumnos/<int:pk>/editar/', views.AlumnoUpdateView.as_view(), name='alumno-update'),
+    path('alumnos/<int:pk>/eliminar/', views.AlumnoDeleteView.as_view(), name='alumno-delete'),
+]
